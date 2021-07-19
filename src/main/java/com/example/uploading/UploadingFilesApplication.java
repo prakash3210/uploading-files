6633d80c0404a57d370515fd53c0419f32477a5e
prakashsharma <prakash3210@gmail.com>
2021-07-19 22:24:41 +0530

    Commit initial

---
+++ src/main/java/com/example/uploading/UploadingFilesApplication.java
@@ -0,0 +1,13 @@
+package com.example.uploading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadingFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadingFilesApplication.class, args);
+	}
+
+}
